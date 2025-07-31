03ebcaa120be4712a3c9e46815b272c94196ed3b
mohanedwalid05 <mohanedwalid05@gmail.com>
2025-07-31 15:38:59 +0300

    add openapi docs

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,9 +1,14 @@
 import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
+import { createOpenAPI, attachFile } from 'fumadocs-openapi/server';
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   // it assigns a URL to your pages
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  pageTree: {
+    attachFile,
+  },
 });
+export const openapi = createOpenAPI();
