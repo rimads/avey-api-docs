d38f985892c9c52af127e142d1a2dbbf9d35a737
mohanedwalid05 <mohanedwalid05@gmail.com>
2025-08-01 13:34:13 +0300

    init

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -3,8 +3,12 @@ import { generateFiles } from "fumadocs-openapi";
 void generateFiles({
   // the OpenAPI schema, you can also give it an external URL.
   input: ["./avey-api.json"],
+
   output: "./content/docs",
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
   includeDescription: true,
+
+
+
 });
