208cadfe816613a8e56ede2d3344dab035ecb385
mohanedwalid05 <mohanedwalid05@gmail.com>
2025-08-04 14:49:19 +0300

    add docker file

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,7 +7,7 @@ export default function ClientNav() {
     <div className="flex items-center  justify-end space-x-4 p-3">
       <Link
         href="/login"
-        className="text-sm font-medium bg-black text-white px-4 py-1.5 rounded hover:bg-gray-800 "
+        className="text-sm font-medium bg-black dark:bg-white text-white dark:text-black px-4 py-1.5 rounded-md hover:bg-gray-800 dark:hover:bg-gray-200"
       >
         Dashboard
       </Link>
