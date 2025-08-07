a905cbae0ee8b32b43c612a058d9b96a3037c3b1
mohanedwalid05 <mohanedwalid05@gmail.com>
2025-08-07 14:38:41 +0300

    update NEW! color

--- src/lib/source.tsx
+++ src/lib/source.tsx
@@ -14,7 +14,6 @@ export const source = loader({
       if (!file) return node;
 
       const data = file.data;
-
       // Use sidebar_title if available, otherwise keep the default title
       if (data.sidebar_title) {
         node.name = data.sidebar_title;
@@ -22,11 +21,11 @@ export const source = loader({
 
       node.name = (
         <>
-          <span className="ml-2 flex justify-between w-full items-center ">
+          <span className="ml-2 flex justify-between w-full items-center -pr-8">
             {node.name}
 
             {data.new && (
-              <span className="mx-1  rounded-full bg-av-blue-500 px-2.5 py-0.5 text-xs font-medium text-fd-background text-right">
+              <span className="ml-1 text-white dark:text-av-blue-100 rounded-full bg-blue-400 px-2.5 py-0.5 text-xs font-medium text-right">
                 NEW!
               </span>
             )}
