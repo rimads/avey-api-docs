e5def0ab45d350ccaebc0a81d6910cce9753981f
mohanedwalid05 <mohanedwalid05@gmail.com>
2025-08-07 18:36:36 +0300

    add experimental files

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -2,8 +2,8 @@ import { generateFiles } from "fumadocs-openapi";
 
 void generateFiles({
   // the OpenAPI schema, you can also give it an external URL.
-  input: ["./avey-api.json"],
-  output: "./content/docs",
+  input: ["./experiment.json"],
+  output: "./content/docs/experiment",
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
   includeDescription: true,
