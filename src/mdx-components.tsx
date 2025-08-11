75eec4e9f07fc99b34f009861916406de9613066
mohanedwalid05 <mohanedwalid05@gmail.com>
2025-08-11 17:42:12 +0300

    update diagnosis content

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,11 +2,13 @@ import defaultComponents from "fumadocs-ui/mdx";
 import { APIPage } from "fumadocs-openapi/ui";
 import { openapi } from "@/lib/source";
 import type { MDXComponents } from "mdx/types";
+import { Mermaid } from "./components/mdx/mermaid";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
+    Mermaid,
     ...components,
   };
 }
